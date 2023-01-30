0c06216099b8952e37846e99e0e6a35b4a1a15fe
Eliyas Kassaye <eliyasson11@gmail.com>
2023-01-30 13:49:13 +0200

    for-silmukkaa

--- loop.c
+++ loop.c
@@ -22,3 +22,29 @@ int main() {
 
     return 0;
 }
+
+/* Tee ohjelma joka kysyy taulukon koon(n) käyttäjältä. Sen jälkeen pyytää käyttäjää antamaan alkoiden arvot. 
+Lopuksi tulostaa ne käänteisessä järjestyksessä. Käytä for-silmukkaa.*/
+
+#include <stdio.h>
+
+int main() {
+    int x;
+    
+    printf("Anna lukujonon pituus  ");
+    scanf("%i", &x);
+    int taulu[x];
+ 
+    for(int i = 0; i < x; i++)
+    { 
+        printf("Anna %i. luku :", i+1);
+        scanf("%i", &taulu[i]);
+    }
+    printf("Lukujono on ");
+    for(int i = 0; i < x; i++){
+        printf("%i ", taulu[i]);
+    }
+    
+
+    return 0;
+}
