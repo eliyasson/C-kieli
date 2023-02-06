b1cfc45e228603e662983752e34f285d5a902c39
Eliyas Kassaye <eliyasson11@gmail.com>
2023-02-06 12:42:58 +0200

    lisää tehtävä

--- lampötila.c
+++ lampötila.c
@@ -28,3 +28,37 @@ int main() {
 
     return 0;
 }
+
+/* Lisää edelliseen tuntitehtävään muunnos jos lämpötila annetaan Fahrenheitina. 
+VINKKI C = (F - 32)/1.8. Testaa kummatkin, kun annetaan Celsius asteet ja Farenheit. */
+
+#include <stdio.h>
+
+int main() {
+    float temp;
+    char c;
+    
+    printf("Fahrenheita (f) vai Celsius (c) ? ");
+    scanf("%c", &c);
+    
+    printf("Anna lampotila: ");
+    scanf("%f", &temp);
+    if (c == 'f','F' ){
+        temp = (temp - 32)/1.8;
+        printf("%.2f C: ", temp);
+    }
+    
+    if(temp < 0){
+        printf("on pakkasta");
+    } else if(temp <= 20) {
+        printf("Suomen kesä");
+    } else if(temp <= 35) {
+        printf("Vihdoinkin lämmin");
+    } else if(temp <= 60) {
+        printf("Liian kuuma");
+    } else {
+        printf("Olet saunassa");
+    };
+
+    return 0;
+}
